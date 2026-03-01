89c5f605ca8553bf5121c58069dd0a5e359409f0
KarlaHerrera15 <162621653+KarlaHerrera15@users.noreply.github.com>
2026-03-01 17:46:16 +0000

    punto 4

--- Prenotazioni/src/app/app.component.ts
+++ Prenotazioni/src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Prenotazione } from './models/prenotazione.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Prenotazioni';
   prenotazioni: Prenotazione[] = [];
+
+constructor(private http: HttpClient) {}
+
+ngOnInit() {
+  this.http.get<Prenotazione[]>(
+    'https://my-json-server.typicode.com/malizia-g/verificaPrenotazioni/prenotazioni'
+  ).subscribe(data => {
+    this.prenotazioni = data;
+  });
+}
 }
